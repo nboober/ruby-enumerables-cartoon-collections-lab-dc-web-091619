433d35a4976cda86820f3e63d000919d26a55ad0
Nicholas Boober <nickboober92@gmail.com>
2019-09-11 16:25:04 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,21 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  i = 0 
+  while i < array.length 
+    puts "#{i + 1}. #{array[i]}"
+    i += 1 
+  end   
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  
+  array.collect{|item| item.capitalize + "!"  }
+  
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+
+  array.any? {|item| item.length > 4}
+
 end
 
 def find_the_cheese# code an argument here
